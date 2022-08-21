bf0a085d6cac502a5f183d85cf80b3947588ecb5
guillermoLop <lopezg35@gmail.com>
2022-08-21 09:24:54 -0300

    prueba commit

---
+++ switch.js
@@ -0,0 +1,14 @@
+let numero=25;
+
+
+for(let numero1=16; numero1<numero; numero1++) {
+    alert("El valor del numero es " +numero1);
+}
+
+for(let i=0; i<10; i++) {
+    alert(i);
+}
+
+for (let i=1; i<=10; i++) {
+    alert(i);
+}
