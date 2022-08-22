695f5d4018bfa56a4d8c53080f06e373a813178c
guillermoLop <lopezg35@gmail.com>
2022-08-22 19:55:07 -0300

    Version 1.0

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+//Calculador de Tonalidades Musicales
+//Desafío opcional N°1 del Curso JS de CoderHouse (Comisión 34010)
+//Alumno: Guillermo Lopez
+
+//Mensaje de bienvenida + primera entrada al ciclo
 let modo = prompt(
   "Bienvenido al Calculador de Tonalidades. Por favor: indique si la tonalidad que busca es Mayor o Menor"
 );
 
+//A lo largo del programa empleo whiles para generar un bucle que evite entradas indeseadas
 while (modo.toLowerCase() != "mayor" && modo.toLowerCase() != "menor") {
   modo = prompt(
     "El dato que ingresó es incorrecto; por favor ingrese 'Mayor' o 'Menor'"
   );
+  console.log("Error: valor ingresado inválido");
 }
 
+//Segunda instancia del ciclo
 let alteracion = parseFloat(
   prompt(
     "Ahora ingrese '1' si la tonalidad no posee alteraciones '2' si posee sostenidos # o bien '3' si tiene bemoles b"
@@ -18,6 +26,7 @@ while (alteracion != 1 && alteracion != 2 && alteracion != 3) {
   alteracion = prompt(
     "Valor incorrecto, por favor ingrese un número del 1 al 3"
   );
+  console.log("Error: valor ingresado inválido");
 }
 
 if (modo == "mayor" && alteracion == 1) {
@@ -38,40 +47,50 @@ if (modo == "mayor" && alteracion == 1) {
     cantDeAlteraciones = parseFloat(
       prompt("Valor incorrecto, por favor ingrese un número del 1 al 7")
     );
+    console.log("Error: valor ingresado inválido");
   }
   switch (cantDeAlteraciones) {
     case (cantDeAlteraciones = 1):
-      let tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} sostenido es Sol Mayor`
       );
+      break;
     case (cantDeAlteraciones = 2):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} sostenidos es Re Mayor`
       );
+      break;
     case (cantDeAlteraciones = 3):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} sostenidos es La Mayor`
       );
+      break;
     case (cantDeAlteraciones = 4):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} sostenidos es Mi Mayor`
       );
+      break;
     case (cantDeAlteraciones = 5):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} sostenidos es Si Mayor`
       );
+      break;
     case (cantDeAlteraciones = 6):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} sostenido es Fa# Mayor`
       );
+      break;
     case (cantDeAlteraciones = 7):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} sostenidos es Do# Mayor`
       );
+      break;
     default:
-      tonalidad = alert(
+      alert(
         `El número que ingresó no es válido; por favor, vuelva a empezar presionando F5`
       );
+      console.log("Error: valor ingresado inválido");
+      break;
   }
 } else if (modo == "mayor" && alteracion == 3) {
   let cantDeAlteraciones = parseFloat(
@@ -86,35 +105,46 @@ if (modo == "mayor" && alteracion == 1) {
   }
   switch (cantDeAlteraciones) {
     case (cantDeAlteraciones = 1):
-      let tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} bemol es Fa Mayor`
       );
+      break;
     case (cantDeAlteraciones = 2):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} bemoles es Sib Mayor`
       );
+      break;
     case (cantDeAlteraciones = 3):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} bemoles es Mib Mayor`
       );
+      break;
     case (cantDeAlteraciones = 4):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} bemoles es Lab Mayor`
       );
+      break;
     case (cantDeAlteraciones = 5):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} bemoles es Reb Mayor`
       );
+      break;
     case (cantDeAlteraciones = 6):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} bemoles es Solb Mayor`
       );
+      break;
     case (cantDeAlteraciones = 7):
-      tonalidad = alert(
+      alert(
         `La tonalidad mayor que posee ${cantDeAlteraciones} bemoles es Dob Mayor`
       );
-          default:
-          tonalidad = alert(`El número que ingresó no es válido; por favor, vuelva a intentarlo presionando F5`)
+      break;
+    default:
+      alert(
+        `El número que ingresó no es válido; por favor, vuelva a intentarlo presionando F5`
+      );
+      console.log("Error: valor ingresado inválido");
+      break;
   }
 } else if (modo == "menor" && alteracion == 2) {
   let cantDeAlteraciones = parseFloat(
@@ -129,37 +159,46 @@ if (modo == "mayor" && alteracion == 1) {
   }
   switch (cantDeAlteraciones) {
     case (cantDeAlteraciones = 1):
-      let tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} sostenido es mi menor`
       );
+      break;
     case (cantDeAlteraciones = 2):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} sostenidos es si menor`
       );
+      break;
     case (cantDeAlteraciones = 3):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} sostenidos es fa# menor`
       );
+      break;
     case (cantDeAlteraciones = 4):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} sostenidos es do# menor`
       );
+      break;
     case (cantDeAlteraciones = 5):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} sostenidos es sol# menor`
       );
+      break;
     case (cantDeAlteraciones = 6):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} sostenido es re# menor`
       );
+      break;
     case (cantDeAlteraciones = 7):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} sostenidos es la# menor`
       );
-      default:
-      tonalidad = alert(
+      break;
+    default:
+      alert(
         `El número que ingresó no es válido; por favor, vuelva a empezar presionando F5`
       );
+      console.log("Error: valor ingresado inválido");
+      break;
   }
 } else if (modo == "menor" && alteracion == 3) {
   let cantDeAlteraciones = parseFloat(
@@ -174,40 +213,50 @@ if (modo == "mayor" && alteracion == 1) {
   }
   switch (cantDeAlteraciones) {
     case (cantDeAlteraciones = 1):
-      let tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} bemol es re menor`
       );
+      break;
     case (cantDeAlteraciones = 2):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} bemoles es sol menor`
       );
+      break;
     case (cantDeAlteraciones = 3):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} bemoles es do menor`
       );
+      break;
     case (cantDeAlteraciones = 4):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} bemoles es fa menor`
       );
+      break;
     case (cantDeAlteraciones = 5):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} bemoles es sib menor`
       );
+      break;
     case (cantDeAlteraciones = 6):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} bemoles es mib menor`
       );
+      break;
     case (cantDeAlteraciones = 7):
-      tonalidad = alert(
+      alert(
         `La tonalidad menor que posee ${cantDeAlteraciones} bemoles es lab menor`
       );
-      default:
+      break;
+    default:
       tonalidad = alert(
         `El número que ingresó no es válido; por favor, vuelva a empezar presionando F5`
       );
+      console.log("Error: valor ingresado inválido");
+      break;
   }
 }
 
+//Mensaje despedida
 let despedida = alert(
   "Muchas gracias. Si desea volver a intentarlo presione F5"
 );
