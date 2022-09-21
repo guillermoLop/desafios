5368d4406fc60d877d5ad96d9b9687f52015e944
guillermoLop <lopezg35@gmail.com>
2022-09-21 17:38:32 -0300

    EVENTOS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //TapeCollector v1.0
 //Alumno: Guillermo Lopez
 
-//Defino la clase Tape con la cual instanciaré los objetos
+//Definiendo clase Tape
 
 class Tape {
   constructor(title, label, year, imdb) {
@@ -12,8 +12,7 @@ class Tape {
   }
 }
 
-//Objetos que representan los artículos de mi colección. Contienen propiedades que no uso en esta entrega pero sí en las que sigan;
-//La idea es manejar un volumen muchísimo mayor de objetos;
+//Objetos Tape
 
 const tape001 = new Tape("Fuga de Absolon", "LK-TEL", 1994, "tt0110678");
 const tape002 = new Tape("Vengador Implacable", "Radiocom", 1981, "tt0084600");
@@ -32,8 +31,7 @@ const tape009 = new Tape("Cut & Run", "LAX", 1984, "tt0089338");
 const tape010 = new Tape("Intento de Fuga", "Bell Video", 1987, "tt0098346");
 const tape011 = new Tape("Demonios 2", "Gativideo", 1986, "tt0090930");
 
-//Armo un array con los objetos definidos previamente;
-//En principio solo manipulo este array, la idea es luego ir incorporando formas de agregar y quitar elementos
+//Array hard-codeado de la colección
 
 const tapes = [
   tape001,
@@ -49,25 +47,9 @@ const tapes = [
   tape011,
 ];
 
-//Inicio de la app. Bienvenida y breve explicación
 
-alert("Bienvenido al TapeCollector");
-alert(
-  "Ud. podrá buscar los títulos de su colección ordenados por Videoeditora"
-);
-
-//Alerta con las posibles opciones
-
-alert(
-  "Actualmente su colección incluye títulos editados por las siguientes empresas:"
-);
 let labelCollection = tapes.map((each) => each.label);
-
-//Este es un método que googlee para eliminar duplicados
-//No entiendo bien cómo funciona pero hace exactamente lo que yo quería
 let filteredLabel = [...new Set(labelCollection)];
-
-alert(filteredLabel.join("\n"));
 
 let labelsHeader = document.getElementById("labelsHeader");
 
@@ -75,44 +57,23 @@ labelsHeader.innerText = "Ud. posee títulos editados por las siguientes videoed
 
 let labelsList = document.getElementById("labels");
 
+let titlesList = document.getElementById("titlesList");
+
 filteredLabel.forEach((labels) => {
   let li = document.createElement("li");
   li.innerText = labels;
   labelsList.appendChild(li);
+  const btn = document.createElement("button")
+  btn.innerHTML = "Ver títulos";
+  labelsList.appendChild(btn);
+  const labelFilter = tapes.filter((tape) => tape.label == labels)
+  let titleFilter = labelFilter.map((tape) => tape.title)
+  let liBtn = document.createElement("li")
+  liBtn.innerHTML = titleFilter.join(", ");
+  btn.onclick = () => {titlesList.appendChild(liBtn)}
 });
 
-let label = prompt(
-  "Ingrese nombre de la videoeditora cuyos títulos desea filtrar"
-).toUpperCase();
 
-const labelFilter = tapes.filter((tape) => tape.label.toUpperCase() == label);
-
-if (labelFilter.length == 0) {
-  alert(`Ud. no posee ningún título de la editora ${label}`);
-  let titles = document.getElementById("titles");
-  titles.innerText = `Ud. no posee ningún título de la editora ${label}`;
-} else if (labelFilter.length == 1) {
-  alert(`Ud. posee el siguiente título de la editora ${label}:`);
-  let titles = document.getElementById("titles");
-  titles.innerText = `Ud. posee el siguiente título de la editora ${label}:`;
-  let labelTitle = labelFilter.map((tape) => tape.title);
-  let titlesList = document.getElementById("titlesList");
-  let li = document.createElement("li");
-  li.innerText = labelTitle;
-  titlesList.appendChild(li);
-} else {
-  alert(
-    `Ud. posee los siguientes ${labelFilter.length} títulos de la editora ${label}:`
-  );
-  let titles = document.getElementById("titles");
-  titles.innerText = `Ud. posee los siguientes ${labelFilter.length} títulos de la editora ${label}:`;
-  let titleFilter = labelFilter.map((tape) => tape.title);
-  let titlesList = document.getElementById("titlesList");
-
-  titleFilter.forEach((titles) => {
-    let li = document.createElement("li");
-    li.innerText = titles;
-    titlesList.appendChild(li);
-  });
-  alert(titleFilter.join("; "));
-}
+//Boton clear para resetear la lista
+let clearBtn = document.getElementById("clear");
+clearBtn.onclick = () => {titlesList.innerHTML = ''};
